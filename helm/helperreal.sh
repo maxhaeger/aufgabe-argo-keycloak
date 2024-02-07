f4795018da11f67f6d3378011704fd6370333966
maxhaeger <maximilian.gross@haeger-consulting.de>
2024-02-07 08:15:25 +0100

    reworked helper script to work flawlessly

--- helm/helperreal.sh
+++ helm/helperreal.sh
@@ -1,68 +1,102 @@
 #!/bin/bash
 
-if ! k3d cluster list | grep -q "k3s-default"; then
-  k3d cluster create --k3s-arg "--disable=traefik@server:*"
-  echo "Wait to set the pods"
-  sleep 20
-fi
+# This script sets up a Kubernetes cluster using k3d, creates the ArgoCD namespace if it doesn't exist,
+# installs or upgrades the environment, waits for the nginx-ingress-controller to be ready,
+# waits for the external IP to be ready, installs or upgrades the frontend, installs or upgrades ArgoCD,
+# syncs the repository with ArgoCD, and installs or upgrades Keycloak.
 
-if $(kubectl get namespace argocd >/dev/null 2>&1); then
-  echo ArgoCD Namespace Exists
-else
-  kubectl create namespace argocd
-fi
+# Command to create a k3d cluster if it doesn't exist
+create_k3d_cluster() {
+  if ! k3d cluster list | grep -q "k3s-default"; then
+    k3d cluster create --k3s-arg "--disable=traefik@server:*"
+    echo "Wait to set the pods"
+    sleep 20
+  fi
+}
 
-if helm status frontend >/dev/null 2>&1; then
-  helm upgrade frontend ./frontend
-else
-  helm install frontend ./frontend
-fi
-echo Frontend Installed/Upgraded
+# Command to create the ArgoCD namespace if it doesn't exist
+create_argocd_namespace() {
+  if $(kubectl get namespace argocd >/dev/null 2>&1); then
+    echo ArgoCD Namespace Exists
+  else
+    kubectl create namespace argocd
+  fi
+}
 
-if helm status environment >/dev/null 2>&1; then
-  helm upgrade environment ./environment
-else
-  helm install environment ./environment
-fi
-echo Environment Installed/Upgraded
+# Command to install or upgrade the environment
+install_or_upgrade_environment() {
+  if helm status environment >/dev/null 2>&1; then
+    helm upgrade environment ./environment
+  else
+    helm install environment ./environment
+  fi
+  echo Environment Installed/Upgraded
+}
 
-external_ip=""
-while [ -z $external_ip ]; do
-  echo "Waiting for end point..."
-  external_ip=$(kubectl get svc ingress-nginx-controller -n ingress-nginx --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
-  [ -z "$external_ip" ] && sleep 10
-done
-echo 'End point ready:' && echo $external_ip
+# Command to wait for nginx-ingress-controller to be ready
+wait_for_ingress_controller() {
+  service="ingress-nginx-controller-admission"
+  namespace="ingress-nginx"
+  endpoints=""
 
-if helm status argocd >/dev/null 2>&1; then
-  helm upgrade argocd ./argocd -n argocd
-else
-  helm install argocd ./argocd -n argocd --create-namespace
-fi
-echo ArgoCD Installed/Upgraded
- sleep 30
- echo Sync Repo with Argo-CRD "Application"
- kubectl apply -f ./argosetup.yml -n argocd
+  while [ -z "$endpoints" ]; do
+    echo "Waiting for $service endpoints to be ready..."
+    endpoints=$(kubectl get endpoints $service -n $namespace -o jsonpath='{.subsets[*].addresses[*].ip}')
+    if [ -z "$endpoints" ]; then
+      sleep 10
+    fi
+  done
 
-if helm status keycloak >/dev/null 2>&1; then
-  helm upgrade keycloak ./keycloak -n keycloak
-else
-  helm install environment ./environment -n keycloak --create-namespace 
-fi
-echo Environment Installed/Upgraded
-# if helm status monitoring >/dev/null 2>&1; then
-#   helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-#   helm upgrade monitoring ./monitoring -n monitoring
-# else
-#  helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-#  helm install monitoring prometheus-community/prometheus -n monitoring
-#  helm install monitoring ./monitoring -n monitoring
-# fi
-# echo Monitoring Installed/Upgraded
+  echo "$service endpoints are ready."
+}
 
-  
+# Command to wait for external IP to be ready
+wait_for_external_ip() {
+  echo Waiting for external IP to be ready...
+  external_ip=""
+  while [ -z $external_ip ] ; do
+    echo "Waiting for end point..."
+    external_ip=$(kubectl get svc ingress-nginx-controller -n ingress-nginx --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+    [ -z "$external_ip" ] && sleep 10
+  done
 
-# kubectl apply -f ../argosetup.yml
+  echo 'End point ready:' && echo $external_ip
+}
 
-# echo ArgoCD configured
+# Command to install or upgrade the frontend
+install_or_upgrade_frontend() {
+  if helm status frontend >/dev/null 2>&1; then
+    helm upgrade frontend ./frontend
+  else
+    helm install frontend ./frontend
+  fi
+  echo Frontend Installed/Upgraded
+}
+
+# Command to install or upgrade ArgoCD
+install_or_upgrade_argocd() {
+  if helm status argocd -n argocd >/dev/null 2>&1; then
+    helm upgrade argocd ./argocd -n argocd
+  else
+    helm install argocd ./argocd -n argocd --create-namespace
+  fi
+  echo ArgoCD Installed/Upgraded
+  sleep 10
+}
+
+# Command to sync the repository with ArgoCD
+sync_repo_with_argocd() {
+  echo Sync Repo with Argo-CRD "Application"
+  kubectl apply -f ./argosetup.yml -n argocd
+}
+
+# Command to install or upgrade Keycloak
+install_or_upgrade_keycloak() {
+  if helm status keycloak -n keycloak >/dev/null 2>&1; then
+    helm upgrade keycloak ./keycloak -n keycloak
+  else
+    helm install keycloak ./keycloak -n keycloak --create-namespace 
+  fi
+  echo Keycloak Installed/Upgraded
+}
 
